b75722eb128c07435f7c63d1eda08a364ebe3fce
Uree1229 <101373376+Uree1229@users.noreply.github.com>
2023-12-06 02:43:01 +0900

    main.h 업데이트

--- main.h
+++ main.h
@@ -1,5 +1,12 @@
 #pragma once
+using namespace std;
 class User_DB;
+
+extern std::string EXTERN_STUDENT_ID;
+static std::string STUDENT_ID;
+static std::string STUDENT_PWD;
 
 void register_menu();
 void login_menu();
+int schedule_main(void);
+
